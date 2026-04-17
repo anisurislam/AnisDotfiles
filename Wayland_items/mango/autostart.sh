08d601c061f31959a84f643376b922dce0256370
Anisur Islam <aianis012355@gmail.com>
2026-04-17 13:43:17 +0600

    clipboard manager added to mango

--- Wayland_items/mango/autostart.sh
+++ Wayland_items/mango/autostart.sh
@@ -1,2 +1,9 @@
+# waybar
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+
+# wallpaper
 swaybg -i ~/Pictures/wallpapers/wallpaper2.jpg >/dev/null 2>&1 &
+
+# clipboard manager
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+wl-paste --type image --watch cliphist store >/dev/null 2>&1 &
